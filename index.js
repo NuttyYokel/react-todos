d08804cef6e30beb1d745e6fe2a26492ecfcb0c2
Marton Cserei <marton.cserei@iquestgroup.com>
2020-01-03 15:47:19 +0200

    implement login toggle

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      isLoggedIn: false
+    };
+    this.toggleLogin = this.toggleLogin.bind(this);
   }
 
   render() {
@@ -11,12 +15,21 @@ class App extends React.Component {
       <section className='app-container'>
         <header>
           <nav>menu items</nav>
-          <a className='login'>Log in</a>
+          <section className='login-container'>
+            {this.state.isLoggedIn && <a className='username'>username</a>}
+            <a className='login' onClick={this.toggleLogin}>
+              {this.state.isLoggedIn ? 'Log out' : 'Log in'}
+            </a>
+          </section>
         </header>
         <main>Some content</main>
         <footer>zi footer</footer>
       </section>
     );
+  }
+
+  toggleLogin() {
+    this.setState(oldState => ({ isLoggedIn: !oldState.isLoggedIn }));
   }
 }
 
